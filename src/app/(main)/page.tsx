8dbc2e09a38e9fc21191ba72036504177813addc
gitFishe <alexshermentalo96@gmail.com>
2026-07-12 11:51:13 +0300

    Add base structure, BetsList component and global styles

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import BetsList from "@/app/(main)/_components/BetsList";
+
+export default function MainPage() {
+    return (
+        <div className='bg-black w-full h-full'>
+            <BetsList/>
+        </div>
+    )
+}
